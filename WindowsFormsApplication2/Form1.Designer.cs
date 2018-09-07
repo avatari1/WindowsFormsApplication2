bd687fb99e27bf2f9df36aeefa5fd8a0aedf07f7
avatari1 <43051973+avatari1@users.noreply.github.com>
2018-09-07 03:02:07 +0300

    Initial commit

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Main_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main_form));
+            this.start_btn = new System.Windows.Forms.Button();
+            this.stop_btn = new System.Windows.Forms.Button();
+            this.folder_field = new System.Windows.Forms.Label();
+            this.choose_btn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.exit_btn = new System.Windows.Forms.Button();
+            this.alphabox = new System.Windows.Forms.TextBox();
+            this.trayicon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Counterlvl = new System.Windows.Forms.Label();
+            this.Counterbox = new System.Windows.Forms.TextBox();
+            this.Counter_btn = new System.Windows.Forms.Button();
+            this.alpha_btn = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // start_btn
+            // 
+            this.start_btn.Location = new System.Drawing.Point(18, 27);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(75, 23);
+            this.start_btn.TabIndex = 0;
+            this.start_btn.Text = "Start";
+            this.start_btn.UseVisualStyleBackColor = true;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
+            // 
+            // stop_btn
+            // 
+            this.stop_btn.Enabled = false;
+            this.stop_btn.Location = new System.Drawing.Point(18, 56);
+            this.stop_btn.Name = "stop_btn";
+            this.stop_btn.Size = new System.Drawing.Size(75, 23);
+            this.stop_btn.TabIndex = 1;
+            this.stop_btn.Text = "Stop";
+            this.stop_btn.UseVisualStyleBackColor = true;
+            this.stop_btn.Click += new System.EventHandler(this.stop_btn_Click);
+            // 
+            // folder_field
+            // 
+            this.folder_field.AutoSize = true;
+            this.folder_field.BackColor = System.Drawing.SystemColors.Control;
+            this.folder_field.Location = new System.Drawing.Point(106, 15);
+            this.folder_field.Name = "folder_field";
+            this.folder_field.Size = new System.Drawing.Size(16, 13);
+            this.folder_field.TabIndex = 4;
+            this.folder_field.Text = "...";
+            // 
+            // choose_btn
+            // 
+            this.choose_btn.Location = new System.Drawing.Point(3, 10);
+            this.choose_btn.Name = "choose_btn";
+            this.choose_btn.Size = new System.Drawing.Size(86, 23);
+            this.choose_btn.TabIndex = 2;
+            this.choose_btn.Text = "Choose Folder";
+            this.choose_btn.UseVisualStyleBackColor = true;
+            this.choose_btn.Click += new System.EventHandler(this.choose_btn_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.choose_btn);
+            this.panel1.Controls.Add(this.folder_field);
+            this.panel1.Location = new System.Drawing.Point(18, 96);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(318, 43);
+            this.panel1.TabIndex = 5;
+            // 
+            // exit_btn
+            // 
+            this.exit_btn.BackgroundImage = global::epayments.Properties.Resources.exit41;
+            this.exit_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.exit_btn.Location = new System.Drawing.Point(280, 27);
+            this.exit_btn.Name = "exit_btn";
+            this.exit_btn.Size = new System.Drawing.Size(56, 49);
+            this.exit_btn.TabIndex = 3;
+            this.exit_btn.UseVisualStyleBackColor = true;
+            this.exit_btn.Click += new System.EventHandler(this.exit_btn_Click);
+            // 
+            // alphabox
+            // 
+            this.alphabox.Enabled = false;
+            this.alphabox.Location = new System.Drawing.Point(18, 155);
+            this.alphabox.Name = "alphabox";
+            this.alphabox.Size = new System.Drawing.Size(135, 20);
+            this.alphabox.TabIndex = 6;
+            this.alphabox.Text = "ΑMPxxxxxxKKKKK";
+            this.alphabox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // trayicon
+            // 
+            this.trayicon.BalloonTipText = "The Application remains open in Tray. DOUble click to restore, right click to clo" +
+    "se";
+            this.trayicon.ContextMenuStrip = this.contextMenuStrip1;
+            this.trayicon.Icon = ((System.Drawing.Icon)(resources.GetObject("trayicon.Icon")));
+            this.trayicon.Text = "epayments renamer";
+            this.trayicon.Visible = true;
+            this.trayicon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.trayicon_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(157, 26);
+            this.contextMenuStrip1.Text = "Onomatizer";
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.exitToolStripMenuItem.Text = "Exit Application";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Counterlvl
+            // 
+            this.Counterlvl.AutoSize = true;
+            this.Counterlvl.Location = new System.Drawing.Point(230, 157);
+            this.Counterlvl.Name = "Counterlvl";
+            this.Counterlvl.Size = new System.Drawing.Size(44, 13);
+            this.Counterlvl.TabIndex = 7;
+            this.Counterlvl.Text = "Counter";
+            // 
+            // Counterbox
+            // 
+            this.Counterbox.Enabled = false;
+            this.Counterbox.Location = new System.Drawing.Point(280, 155);
+            this.Counterbox.Name = "Counterbox";
+            this.Counterbox.Size = new System.Drawing.Size(42, 20);
+            this.Counterbox.TabIndex = 8;
+            this.Counterbox.Text = "1";
+            this.Counterbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Counter_btn
+            // 
+            this.Counter_btn.Location = new System.Drawing.Point(324, 152);
+            this.Counter_btn.Name = "Counter_btn";
+            this.Counter_btn.Size = new System.Drawing.Size(40, 23);
+            this.Counter_btn.TabIndex = 9;
+            this.Counter_btn.Text = "Edit";
+            this.Counter_btn.UseVisualStyleBackColor = true;
+            this.Counter_btn.Click += new System.EventHandler(this.Counter_btn_Click);
+            // 
+            // alpha_btn
+            // 
+            this.alpha_btn.Location = new System.Drawing.Point(159, 152);
+            this.alpha_btn.Name = "alpha_btn";
+            this.alpha_btn.Size = new System.Drawing.Size(40, 23);
+            this.alpha_btn.TabIndex = 10;
+            this.alpha_btn.Text = "Edit";
+            this.alpha_btn.UseVisualStyleBackColor = true;
+            this.alpha_btn.Click += new System.EventHandler(this.alpha_btn_Click);
+            // 
+            // Main_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 189);
+            this.Controls.Add(this.alpha_btn);
+            this.Controls.Add(this.Counter_btn);
+            this.Controls.Add(this.Counterbox);
+            this.Controls.Add(this.Counterlvl);
+            this.Controls.Add(this.alphabox);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.exit_btn);
+            this.Controls.Add(this.stop_btn);
+            this.Controls.Add(this.start_btn);
+            this.Name = "Main_form";
+            this.Text = "epayment renamer";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start_btn;
+        private System.Windows.Forms.Button stop_btn;
+        private System.Windows.Forms.Button exit_btn;
+        private System.Windows.Forms.Label folder_field;
+        private System.Windows.Forms.Button choose_btn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox alphabox;
+        private System.Windows.Forms.NotifyIcon trayicon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label Counterlvl;
+        private System.Windows.Forms.TextBox Counterbox;
+        private System.Windows.Forms.Button Counter_btn;
+        private System.Windows.Forms.Button alpha_btn;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,443 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Permissions;
+using System.Reflection;
+
+namespace WindowsFormsApplication2
+{
+    
+    public partial class Main_form : Form
+    {
+
+        public string sourcepath="";
+        public string destfile="";
+        public Main_form()
+        {
+
+
+            InitializeComponent();
+
+        }
+        public bool startpressed;
+        private void choose_btn_Click(object sender, EventArgs e)
+        {
+            var fod = new FolderBrowserDialog();
+            if (fod.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                folder_field.Text = fod.SelectedPath;
+            }
+            else
+            {
+                MessageBox.Show("A folder has not been selected");
+            }
+        }
+
+        private static FileSystemWatcher _watcher;
+
+        public void settings_save()
+        {
+            set_settings_value("f", folder_field.Text);
+            set_settings_value("a", alphabox.Text);
+            set_settings_value("c", Counterbox.Text);
+        }
+        protected virtual bool IsFileLocked(FileInfo file)
+        { 
+
+            FileStream stream = null;
+
+            try
+            {
+                stream = file.Open(FileMode.Open, FileAccess.Read, FileShare.None);
+            }
+            catch (IOException)
+            {
+                //the file is unavailable because it is:
+                //still being written to
+                //or being processed by another thread
+                //or does not exist (has already been processed)
+                return true;
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+            }
+
+            //file is not locked
+            return false;
+        }
+
+        public void set_settings_value(string flag, string value)
+        {
+            string settings_file = Directory.GetCurrentDirectory() + "\\" + "settings" + "\\" + "settings.ini";
+            List<string> items1 = new List<string>();
+            string path1 = settings_file;
+            try
+            {
+                FileInfo fi1 = new FileInfo(path1);
+
+                if (!IsFileLocked(fi1))
+                {
+
+
+                    using (StreamReader reader = File.OpenText(path1))
+                    {
+                        string line;
+
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            items1.Add(line);
+                        }
+
+                        switch (flag)
+                        {
+                            case "f":
+                                items1[0] = value;
+                                break;
+                            case "a":
+                                items1[1] = value;
+                                break;
+                            case "c":
+                                items1[2] = value;
+                                break;
+                            case "d":
+                                items1[3] = value;
+                                break;
+                            default:
+                                break;
+                        }
+
+                        reader.Dispose();
+                    }
+                    
+                }
+            }
+            catch 
+            {
+                //MessageBox.Show(ex.Message);
+
+            }
+            string filesaver = "";
+            foreach (string item in items1)
+            {
+                filesaver += item + Environment.NewLine;
+            }
+            try
+            {
+                string newdir = Directory.GetCurrentDirectory() + "\\" + "settings";
+                FileInfo fi1 = new FileInfo(path1);
+                if (!IsFileLocked(fi1))
+                {
+                    if (!Directory.Exists(newdir))
+                    {
+                        var di = new DirectoryInfo(Directory.GetCurrentDirectory());
+                        di.Attributes &= ~FileAttributes.ReadOnly;
+                        Directory.CreateDirectory(newdir);
+                    }
+                    else
+                    {
+                        var di = new DirectoryInfo(newdir);
+                        di.Attributes &= ~FileAttributes.ReadOnly;
+                    }
+                    using (FileStream fs = File.OpenWrite(settings_file))
+                    {
+
+                        Byte[] info = new UTF8Encoding(true).GetBytes(filesaver);
+
+                        // Add some information to the file.
+                        fs.Write(info, 0, info.Length);
+                        fs.Dispose();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("could not create settings file" + Environment.NewLine + "original message : " + ex.Message);
+            }
+        }
+
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+            if (folder_field.Text != "...")
+            {
+                start_btn.Enabled = false;
+                alpha_btn.Enabled = false;
+                Counter_btn.Enabled = false;
+                choose_btn.Enabled = false;
+
+                stop_btn.Enabled = true;
+
+
+                var foldername = folder_field.Text;
+                string settings_file = Directory.GetCurrentDirectory()+"\\"+"settings";
+                settings_save();
+
+                // Create a new FileSystemWatcher and set its properties.
+                _watcher = new FileSystemWatcher
+                {
+
+                    Path = folder_field.Text,
+                    IncludeSubdirectories = true,
+                    InternalBufferSize = 400000,
+                    NotifyFilter = NotifyFilters.LastWrite
+               //     |NotifyFilters.FileName
+                    
+
+                };
+
+                _watcher.Filter = "AMP*.xml";
+                // Add event handlers
+                _watcher.Changed += new FileSystemEventHandler(OnChanged);
+                _watcher.Created += new FileSystemEventHandler(OnCreated);
+                _watcher.Renamed += new RenamedEventHandler(OnRenamed);
+                // Begin watching
+                _watcher.EnableRaisingEvents = true;
+
+            }
+            else
+            {
+                MessageBox.Show("Please select a folder first");
+            }
+        }
+        DateTime lastRead = DateTime.MinValue;
+        private void OnCreated(object sender, FileSystemEventArgs e)
+        {
+         //   MessageBox.Show(" oncreated event");
+            string source_file = e.FullPath;
+            string destination_file = filename_gen();
+            go(source_file, destination_file);
+        }
+       
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+          //  MessageBox.Show(" onchanged event");
+            string source_file = e.FullPath;
+            string destination_file = filename_gen();
+            go(source_file, destination_file);
+        }
+
+
+        private void OnRenamed(object source, FileSystemEventArgs e)
+        {
+         //   MessageBox.Show(" onrenamed event");
+            string source_file = e.FullPath;
+            string destination_file = filename_gen();
+            go(source_file, destination_file);
+        }
+        public void go(string _source, string _dest)
+        {
+
+            // MessageBox.Show("source : "+ source_file +Environment.NewLine + "Destination : " +destination_file);
+            try
+            {
+                System.IO.File.Move(_source, _dest);
+                int x= int.Parse(get_counterbox());
+                x++;
+                set_counterbox(x.ToString());
+          //      MessageBox.Show("x is " + x.ToString() + Environment.NewLine + "str is " + str);
+                set_settings_value("d", DateTime.Now.Date.ToString());
+            }
+            catch (Exception ex)
+            {
+             MessageBox.Show("there was a problem " + ex.Message);                                       ///to be removed
+            }
+        }
+        private void stop_btn_Click(object sender, EventArgs e)
+        {
+            start_btn.Enabled = true;
+            alpha_btn.Enabled = true;
+            Counter_btn.Enabled = true;
+            choose_btn.Enabled = true;
+
+            stop_btn.Enabled = false;
+            _watcher.EnableRaisingEvents = false;
+        }
+
+     
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            Hide();
+        }
+
+        private void exit_btn_Click(object sender, EventArgs e)
+        {
+            
+            // Confirm user wants to close
+            switch (MessageBox.Show(this, "Are you sure you want to Exit the Application?", "Closing", MessageBoxButtons.YesNo))
+            {
+                case DialogResult.No:
+                    break;
+                default:
+                    this.Dispose();
+                    Environment.Exit(0);
+                    break;
+            }
+
+            
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+            Show();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+                
+        public string filename_gen()           // create destination filename
+        {
+
+            string chosen_folder = get_folderfield();
+            string dest_filename = get_alphabox();
+            string cntstr = Counterbox.Text.ToString();
+         //   string settings_file = Directory.GetCurrentDirectory() + "\\" + "settings";
+         //   string path1 = settings_file;
+         //   foo foo1 = new foo();
+         //   foo1.settings_load(out chosen_folder, out dest_filename, out cntstr);
+
+
+            string ys = System.DateTime.Now.Year.ToString();
+            string ms = System.DateTime.Now.Month.ToString();
+            string ds = System.DateTime.Now.Day.ToString();
+            // counter needs to be 3 digits
+            string s2 = chosen_folder + "\\" + dest_filename + ys + str_dig(ms,2) + str_dig(ds,2) + str_dig(cntstr,3) + "_pain001.xml";
+            return s2;
+        }
+
+
+
+        public string get_folderfield()
+        {
+            return folder_field.Text;
+        }
+        public string get_alphabox()
+        {
+            return alphabox.Text;
+        }
+        public string get_counterbox()
+        {
+            return Counterbox.Text;
+        }
+        
+
+        public void set_counterbox(string _value)
+        {
+             // running on worker thread
+            this.Invoke((MethodInvoker)delegate {
+                Counterbox.Text = _value; // runs on UI thread
+            });
+            
+        }
+
+        private void Counter_btn_Click(object sender, EventArgs e)
+        {
+            string initial_string = "1";
+            if (Counterbox.Enabled == false)
+            {
+                Counterbox.Enabled = true;
+                Counter_btn.Text = "Ok";
+                initial_string = Counterbox.Text; // save the previous value temporarilly
+            }
+            else
+            {
+ 
+                // bool result = Int32.TryParse(Counterbox.Text.ToString(), out x);
+                bool result = System.Text.RegularExpressions.Regex.IsMatch(Counterbox.Text.ToString(), @"^\d+$");
+                if (!result)
+                {
+                    set_counterbox(initial_string); // if the parsing fails return messsage and the previous value
+                    MessageBox.Show("This field only accepts numbers");
+                }
+
+                Counterbox.Enabled = false;
+                Counter_btn.Text = "Edit";
+            }
+        }
+        public string get_settings_value(string flag)
+        {
+            string settings_file = Directory.GetCurrentDirectory() + "\\" + "settings" + "\\" + "settings.ini";
+
+            string path1 = settings_file;
+            try
+            {
+                using (StreamReader reader = File.OpenText(path1))
+                {
+                    string line;
+                    List<string> items1 = new List<string>();
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        items1.Add(line);
+                    }
+
+                    switch (flag)
+                    {
+                        case "f":
+                            return items1[0];
+                        case "a":
+                            return items1[1];
+                        case "c":
+                            return items1[2];
+                        case "d":
+                            return items1[3];
+                        default:
+                            return "";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to load settings file" + Environment.NewLine + "original message : " + ex.Message);
+                return "";
+            }
+        }
+        delegate void SetTextCallback(string text);
+        public string str_dig(string _input,Int32 digitsneeded)
+        {
+            Int32 numofaddzeros = 0;
+            numofaddzeros = digitsneeded - _input.Length;
+            switch (numofaddzeros)
+            {
+                case 1:
+                    return "0" + _input;
+                case 2:
+                    return "00" + _input;
+                default:
+                    return _input;
+            }
+        }
+
+
+
+        private void trayicon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+        }
+
+        private void alpha_btn_Click(object sender, EventArgs e)
+        {
+            if (alphabox.Enabled)
+            {
+                alpha_btn.Text = "Edit";
+                alphabox.Enabled = false;
+            }
+            else
+            {
+                alpha_btn.Text = "Save";
+                alphabox.Enabled = true;
+            }
+
+                      }
+    }
+}
